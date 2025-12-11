c8f911ac79d21fabe1b06320d453aebaae5f1f74
Florian <94357651+Flyrik@users.noreply.github.com>
2025-12-11 21:36:58 +0100

    AjoutQuete1

--- Projet-AR/Assets/Script/PointManager.cs
+++ Projet-AR/Assets/Script/PointManager.cs
@@ -3,15 +3,35 @@ using TMPro;
 
 public class PointManager : MonoBehaviour
 {
-
-    public static int point = 0;
+    public static PointManager instance;
+    public static int score = 0;
     public TextMeshProUGUI points;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+     private void Awake()
+    {
+        // Singleton
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+    
     void Start()
     {
-        points.text = point.ToString("F0");
+        points.text = score.ToString("F0");
         
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+        UpdateScoreUI();
+    }
+    private void UpdateScoreUI()
+    {
+        if (points != null)
+            points.text = score.ToString("F0");
     }
 
     // Update is called once per frame
--- Projet-AR/Assets/Script/Quetescritp.cs
+++ Projet-AR/Assets/Script/Quetescritp.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class Quetescritp : MonoBehaviour
 {
@@ -9,31 +10,74 @@ public class Quetescritp : MonoBehaviour
     public VuforiaObject archer;
     public VuforiaObject Rage;
     public TextMeshProUGUI quetes1grogu;
+    public AudioSource recompense;
 
+    public TextMeshProUGUI quete2Text;
+
+     private bool quete1Done = false;
+    private bool quete2Done = false;
+
+    private bool quete3Done = false;
+
+    public GameObject QueteOBJ1;
+    public GameObject QueteOBJ2;
 
 
     void Start()
     {
-
+        quete2Done = false;
+        quete1Done = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (chevalier.isDetected == true)
+         if (!quete1Done) // elle n’est PAS encore faite
         {
-            quetes1grogu.text = "Afficher";
-            quetes1grogu.color = Color.green;
+            if (chevalier.isDetected == true)
+            {
+                quete1Done = true;
+                quetes1grogu.text = "Afficher";
+                quetes1grogu.color = Color.green;
+                PointManager.instance.AddScore(20);
+                StartCoroutine(Attendre());
+                recompense.Play();
+                
+                
 
+            }
+            else
+            {
+                quetes1grogu.text = "Non Afficher";
+                quetes1grogu.color = Color.red;
+
+
+
+            }
         }
-        else
+         if (quete1Done && !quete2Done)  // La quête 2 ne commence que si la 1 est faite
         {
-            quetes1grogu.text = "Non Afficher";
-            quetes1grogu.color = Color.red;
+            if (archer.isDetected & chevalier.isDetected)
+            {
+                quete2Done = true;
+                quete2Text.text = "Afficher";
+                quete2Text.color = Color.green;
+                PointManager.instance.AddScore(30);
 
-
-
+            }
+            else
+            {
+                quete2Text.text = "Non Afficher";
+                quete2Text.color = Color.red;
+            }
         }
     }
+    IEnumerator Attendre()
+{
+    yield return new WaitForSeconds(2);
+    QueteOBJ1.SetActive(false);
+                QueteOBJ2.SetActive(true);
+}
+
 
 }
